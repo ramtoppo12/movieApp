4af0efb880c2fdd4ad9ca9ab5a9bbc7151f14b58
Ram kumar toppo <ramtoppo123321@gmail.com>
2021-01-20 17:59:41 +0530

    Image logo changed

--- src/Navbar.js
+++ src/Navbar.js
@@ -17,7 +17,7 @@ export default function Navbar() {
     },[])
     return (
         <div className={`navbar ${show && "nav__black"}`}>
-            <img className="nav__logo" src="https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fwww.tubefilter.com%2Fwp-content%2Fuploads%2F2016%2F07%2FNetflix_logo.jpg&f=1&nofb=1.jpg" alt="Netfix official logo"/>
+            <img className="nav__logo" src="../assets/img/logo.png" alt="Netfix official logo"/>
 
         <button className="nav__avatar">Sign in</button>
 
